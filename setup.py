0209751d2084b5221021f11925e975579c069e65
Emily Stephen <emilyps14@gmail.com>
2023-05-23 22:24:49 -0400

    Setup and environment

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='Common_Oscillator_Models',
+    version='',
+    packages=['core_functions'],
+    url='',
+    license='',
+    author='',
+    description='',
+)
